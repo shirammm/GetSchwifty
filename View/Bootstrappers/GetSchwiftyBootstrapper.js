7dff480a41c4eeb3bb517c7531c2bd2e22abfe9c
shirammm <Shira02.md@gmail.com>
2022-07-04 17:00:36 +0300

    update bootstrapper with new feature

--- View/Bootstrappers/GetSchwiftyBootstrapper.js
+++ View/Bootstrappers/GetSchwiftyBootstrapper.js
@@ -4,6 +4,8 @@ import { clickOnButton } from "../Elements/Buttons/ClickOnButton.js";
 import { isGameOver } from "../GameOver/GameOverChecker.js";
 import { hideTheLastCell } from "../Initializations/HiddenCell/LastCellHider.js";
 import { generateNumericalColorfulBackground } from "../Initializations/Backgrounds/NumericalColorfulBackground.js";
+import { shuffle } from "../Restart/Shuffler.js";
+import { isSolveable } from "../Valids/ValidSolveableBoard.js";
 import { GetSchwiftyManager } from "../GameManager/GetSchwiftyManager.js";
 
 var rows = 3;
@@ -11,15 +13,19 @@ var columns = 3;
 
 let initializeTableFundamentals = createTableFundamentals;
 let initializeElements = createButtonsAsTableElements;
-let clickOnElementFunction = clickOnButton;
+let clickOnElement = clickOnButton;
 let initializeElementsBackground = generateNumericalColorfulBackground;
 let hideUnnecessaryCells = hideTheLastCell;
 let gameOverChecker = isGameOver;
+let shuffler = shuffle;
+let checkTableValidity = isSolveable;
 
 let gameManager = new GetSchwiftyManager(initializeTableFundamentals,
                                         initializeElements,
-                                        clickOnElementFunction,
+                                        clickOnElement,
                                         initializeElementsBackground,
                                         hideUnnecessaryCells,
-                                        gameOverChecker);
+                                        gameOverChecker,
+                                        shuffler,
+                                        checkTableValidity);
 gameManager.start(rows, columns);
