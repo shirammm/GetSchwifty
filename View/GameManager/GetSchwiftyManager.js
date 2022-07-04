636910a29a62a72bbaf87bdfce190028cd4bc081
shirammm <Shira02.md@gmail.com>
2022-07-04 17:01:07 +0300

    update game manager with new feature

--- View/GameManager/GetSchwiftyManager.js
+++ View/GameManager/GetSchwiftyManager.js
@@ -4,20 +4,30 @@ export class GetSchwiftyManager {
                  clickOnElementFunction,
                  initializeElementsBackground,
                  hideUnnecessaryCells,
-                 gameOverChecker){
+                 gameOverChecker,
+                 shuffler,
+                 checkTableValidity){
         this.initializeTableFundamentals = initializeTableFundamentals;
         this.initializeElements = initializeElements;
         this.clickOnElementFunction = clickOnElementFunction;
         this.initializeElementsBackground = initializeElementsBackground;
         this.hideUnnecessaryCells = hideUnnecessaryCells;
         this.gameOverChecker = gameOverChecker;
+        this.shuffler = shuffler;
+        this.checkTableValidity = checkTableValidity;
     }
 
     start(rows, columns) {
         let table = this.initializeTableFundamentals(rows, columns);
-        console.log(...table);
-    this.initializeElements(table, this.clickOnElementFunction, this.gameOverChecker);
-    this.initializeElementsBackground(table);
-    this.hideUnnecessaryCells(table);
+        // while(this.checkTableValidity(table) == false){
+        //     this.shuffler(table);
+        // }
+        do {
+            this.shuffler(table);
+            console.log("again")
+        } while(!this.checkTableValidity(table));
+        this.initializeElements(table, this.clickOnElementFunction, this.gameOverChecker);
+        this.initializeElementsBackground(table);
+        this.hideUnnecessaryCells(table);
     }
 }
