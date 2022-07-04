3e8d113dba4556d91f4428ecd945f097364b4b4d
shirammm <Shira02.md@gmail.com>
2022-07-04 15:13:49 +0300

    game board initializer

---
+++ View/Initializations/Table/BoardGenerator.js
@@ -0,0 +1,25 @@
+import { TableCell } from "../../../Model/Assets/TableCell.js";
+
+function generateRandomInt(maxLimit) {
+    let rand = Math.random() * maxLimit;
+    rand = Math.floor(rand);
+    return rand;
+}
+
+export function createTableFundamentals(rows, columns) {
+    let optionalNumbers = [...Array(rows*columns).keys()];
+    console.log(...optionalNumbers)
+    let table = [];
+    for(let i=0; i<rows; i++) {
+        let row = [];
+        for(let j=0; j<columns; j++) {
+            let randomIndex = generateRandomInt(optionalNumbers.length);
+            let elementDesignatedLocation = optionalNumbers[randomIndex];
+            let cell = new TableCell(i*rows+j, {}, elementDesignatedLocation);
+            row.push(cell);
+            optionalNumbers.splice(randomIndex, 1);
+        }
+        table.push(row);
+    }
+    return table;
+}
